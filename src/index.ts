2a7dd4fc4f9458cbf8bde3cea4b01f9d8b6548e2
ajshan23 <ajmalshahan23@gmail.com>
2025-11-13 18:16:47 +0530

    changed port

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 //   res.sendFile("/var/www/kmcc-frontend/dist/index.html");
 // });
 connectDb().then(() => {
-  app.listen(40034, () => {
+  app.listen(4001, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
 });
